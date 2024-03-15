1908ec25831d8796c31d86ffce51b74494b44b2e
viniciosmalaquias <105360594+viniciosmalaquias@users.noreply.github.com>
2024-03-15 08:35:02 -0300

    ok

---
+++ main.js
@@ -0,0 +1,14 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+//laco de repeticao
+for(let i=0; i <botoes.length; i++){
+    botoes[i].onclick = function(){
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
