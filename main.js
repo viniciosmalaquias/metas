66b45cf7b3dcbaed0df001d739ec7a69c5084171
viniciosmalaquias <105360594+viniciosmalaquias@users.noreply.github.com>
2024-04-12 08:44:27 -0300

    ok

--- main.js
+++ main.js
@@ -14,6 +14,10 @@ for(let i=0; i <botoes.length; i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-02-02T00:00:00");
+const tempoObjetivo2 = new Date("2024-02-02T00:00:00");
+const tempoObjetivo3 = new Date("2024-02-02T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-02T00:00:00");
+
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
